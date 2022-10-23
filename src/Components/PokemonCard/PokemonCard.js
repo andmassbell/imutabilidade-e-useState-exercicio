262cd6a53ee992f17d7d5c5012468ee49c450bdb
andmassbell <andremassenabelo@gmail.com>
2022-10-23 18:17:37 -0300

    exercicio-imutabilidade-feito

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,22 +1,47 @@
 import React from 'react'
-import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
+import { Card, PokemonName, PokemonType, EvolveButton } from './styles'
 
 
 
 const PokemonCard = (props) => {
 
-    const evoluirPokemon = () => {
-        console.log("Cliquei no botão de evoluir")
+  const evoluirPokemon = () => {
+    if (props.pokemon.name === 'Pichu') {
+      props.setPokemon({
+        ...props.pokemon,
+        name: "Pikachu",
+        evolved: true,
+        weight: 6,
+        image: 'https://archives.bulbagarden.net/media/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png',
+      })
+    } else if (props.pokemon.name === 'Bronzor') {
+      props.setPokemon1({
+        ...props.pokemon1,
+        name: "Bronzong",
+        evolved: true,
+        weight: 187,
+        image: 'https://archives.bulbagarden.net/media/upload/thumb/a/aa/437Bronzong.png/250px-437Bronzong.png'
+      })
+    } else if (props.pokemon.name === 'Jigglypuff'){
+      props.setPokemon2({
+        ...props.pokemon2,
+        name: 'Wigglytuff',
+        evolved: true, 
+        weight: 12,
+        image: 'https://archives.bulbagarden.net/media/upload/thumb/9/92/040Wigglytuff.png/250px-040Wigglytuff.png'
+      })
     }
-    
-  return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+  }
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+
+  return (
+    <Card color={props.pokemon.color}>
+      <img src={props.pokemon.image} alt={`Pokemon`} />
+      <PokemonName>{props.pokemon.name}</PokemonName>
+      <PokemonType>{props.pokemon.type}</PokemonType>
+      <p>{props.pokemon.weigth}kg</p>
+
+      <EvolveButton onClick={() => evoluirPokemon(props.setPokemon, props.setPokemon1, props.setPokemon2)}>Evoluir!</EvolveButton>
     </Card>
   )
 }
--- src/Components/PokemonCard/styles.js
+++ src/Components/PokemonCard/styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  background-color: ${(props) => props.color};
-  border: 1px solid black;
-  margin: 16px;
-  max-width: 17rem;
-  text-align: center;
-  border-radius: 10px;
-  box-shadow: 10px 10px 30px #bdbdbd;
+    background-color: ${(props) => props.color};
+    border: 1px solid black;
+    margin: 190px;
+    max-width: 17rem;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 20px 10px 30px #bdbdbd;
 `;
 
 export const PokemonName = styled.h2`
@@ -23,7 +23,7 @@ export const PokemonType = styled.p`
 export const EvolveButton = styled.button`
     padding: 0.5rem;
     margin: 16px;
-    border-radius: 4px;
+    border-radius: 10px;
     border: none;
     cursor: pointer;
     box-shadow: 0 6px 30px -10px #000000;
